1515ba6744a936b558e0267a57ebd9799cb0915b
SLocs123 <sam@thelockyers.net>
2024-03-25 14:28:11 +0000

    todo: assign unassigned tracks, delete/retire old tracks
    Once done structure is complete

--- QT/QuickTrack.py
+++ QT/QuickTrack.py
@@ -6,56 +6,66 @@ from colormath.color_conversions import convert_color
 import sys
 import cv2
 import random
+import warnings
 
 # implementation of SAE - https://github.com/starwit/sae-stage-template
 
 
-class QuickTrack: # Quicktrack class contains all image parameters and the maximum tolerances for tracking properties. It also conatains lists of all tracks and tracklets
-    def __init__(self, help=False, classPath='QT/default.names', threshold=0.7, maxDisplacement=[150, 100], maxColourDif=2000, maxShapeDif=0.5, weights=[15, 2, 2], maxAge=6, colour='no', vitalScale=0.7, assign='greedy'):
+class QuickTrack:
+    def __init__(self, classPath: str='QT/default.names', threshold: float=0.7, maxDisplacement: list[int]=[150, 100], maxColourDif: int=2000, maxShapeDif: float=0.5, weights: list[int]=[15, 2, 2], maxAge: int=6, colour: str='no', vitalScale: float=0.7, assign: str='greedy'):
+        """
+        img is the current frame being inferenced, this also needs to be passed into the update function
+        thres is the confidence threshold that gates a track-tracklet conf. The conf must be higher than this value
+        the max variables determine a gradient for confidence calculations, these will be tuned for stationary traffic camera conditions, but changing them could help for varying scenarios - Implement adaptable max disp!!!!!!!!!!!!!
+        weights represents the weighting of the confidence fundtions. These are called in the _calculateWeightedConfidence function
+        classes must point to class list that the inference uses, defaut is the coco set
+        tracks ----------------------------
+        trackCount is used to name new tracks, it doesn't represent the number of LIVE tracks ut the total tracks created
+        tracklets
+        trackletCount, same as above
+        maxAge determines the maximum age of a track withoutbeing updated, once this is passed the track will be removed/deactivated
+        frame is the number of frames, not the img. 
+        colour is the colour method preference, |none is recomended atm|
+        vitalScale is the value which the confidence is scaled down by when vital functions aren't fulfilled |subject to change|
+        assign is the method used to assign tracks-tracklets
+        colours is used to show bbox, unique for each class
 
-        if help:
-            print('required params.txt .......') # -----------------------------------------------------Finish, or print a txt file---------------------------------------------------#
-        self.img = None # the img is the current frame being inferenced, needs to be updated every frame in the update function
-        # self.imgH = None # img used for distance estimation, not necassary unless distance is reintroduced
-        self.thres = threshold # confidence threshold for tracklet to be assigned as potential to a track
-        self.maxDisp = maxDisplacement # maximum [x, y] displacement from predicted position, allows creatiion of a gradient for confidence calc
-        self.MaxColDif = maxColourDif # same as previous but for colour, not relevant when using ML method, simple is not reliable
-        self.maxShapeDif = maxShapeDif # same as previous, but for object shape, or aspect ratio
-        self.weights = weights # Weights is the input to adjust how different confidence factors are weighted in the average, check conf.py for conf functions, then input weights in same order for preference
-        self.classes = load_classes(classPath) # Allows the loading of classes, allows translation from inference class integer to wording. This file must allign with the inference type e.g. coco.names
-        self.tracks = [] # These are the confirmed tracks, could look at using sets!!!!!!!!!!!!!!!!!
-        self.trackCount = 0 # allows unique naming of tracks
-        self.tracklets = [] # These are tracklets created from detection list, could look at using sets!!!!!!!!!!!!!!!!!
-        self.trackletCount = 0 # allows unique naming of tracklets
-        self.maxAge = maxAge # determines how old a track can be without an update before being removed
-        self.frame = 0 # used to track how old the sequence is
-        self.colour = colour # this is the colour method preference, none is recomended atm
+        POTENTIAL CHANGES:
+        Look at using sets or numpy arrays for the tracks and tracklets, to try and increase loop speeds
+        np arrays for colour theory parts
+        """
+        if colour not in {'no', 'simple', 'ML'}:
+            warnings.warn(f"Invalid colour '{colour}'. Defaulting to 'no'.", RuntimeWarning)
+            self.colour = 'no'
+        else:
+            self.colour = colour
+        if assign not in {'greedy', 'linSum'}:
+            warnings.warn(f"Invalid assign method '{assign}'. Defaulting to 'greedy'.", RuntimeWarning)
+            self.assign = 'greedy'
+        else:
+            self.assign = assign  
+
+        self.img = None 
+        self.thres = threshold
+        self.maxDisp = maxDisplacement 
+        self.maxColDif = maxColourDif 
+        self.maxShapeDif = maxShapeDif 
+        self.weights = weights 
+        self.classes = load_classes(classPath)
+        self.tracks = []
+        self.trackCount = 0
+        self.tracklets = []
+        self.trackletCount = 0
+        self.maxAge = maxAge
+        self.frame = 0 
         self.vitalScale = vitalScale
-        self.assign = assign
         self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.classes]
 
-                # Get only functions from util.py that start with 'conf_'
-        # self.confMetrics = [func for func in dir(util) if callable(getattr(util, func)) and func.startswith("conf_")]
-        # # Identify vital functions that start with 'confVital_'
-        # self.vitalFunctions = [func for func in dir(util) if callable(getattr(util, func)) and func.startswith("confVital_")]
-
-    def _generateInitialTracks(self, detectionList):
-        tracks = []
-        for detection in detectionList:
-            # distance = self.getDistance(detection) # -----------------------------# define initial measurements for a track
-            # colour = self.getColour(detection)
-            if self.colour == 'ML':
-                colour = getColourML(detection, self.img)
-            if self.colour == 'Simple':
-                colour = getColourSimple(detection, self.img)
-            else:
-                colour = None   
-            newTrack = Tracks(self.trackCount, detection, 0, colour)
-            tracks.append(newTrack)
-            self.trackCount += 1
-        self.tracks = tracks    
-
     def update(self, detectionList, img):
+        """
+        This is the main function while running, will process all other operations in the tracker.
+        return is alligned to the SAE benchmarking format: https://github.com/starwit/object-tracker-benchmark/tree/main
+        """
         self._updateFrame(img)
         if self.frame == 0:
             self._generateInitialTracks(detectionList)
@@ -64,60 +74,71 @@ class QuickTrack: # Quicktrack class contains all image parameters and the maxim
             self._updateTracks()
         self.frame += 1
 
+        out = []
+        for track in self.tracks:
+            out.append([track.bbox[0],track.bbox[1], track.bbox[2], track.bbox[3], track.Id, track.conf, track.cls, 1])
+        return out
+
+
     def show(self):
+        """
+        Show will output a video stream showing all tracks labelled in the inference/input video
+        Uses the yolo format/approach
+        """
         for track in self.tracks:
             xyxy = track.bbox
             label = '%s | %d' % (self.classes[track.cls], track.Id)
             self.plot_one_box(xyxy, self.img, label=label, color=self.colors[track.cls], line_thickness=3)
         cv2.imshow('Output', self.img)
-        cv2.waitKey(1)  # 1 millisecond
+        cv2.waitKey(1)  # 1 millisecond, 0 for a keypress
+
+    def _generateInitialTracks(self, detectionList):
+        """
+        Called for the first frame only, this function will convert all detections into tracks instead of tracklets
+        """
+        tracks = []
+        for detection in detectionList:
+            colour = self._getColour(detection)
+            newTrack = Tracks(self.trackCount, detection, 0, colour)
+            tracks.append(newTrack)
+            self.trackCount += 1
+        self.tracks = tracks        
 
     def _updateFrame(self, img):
         self.img = img
-        # self.imgH, _, _ = img.shape
         self.tracklets = []
         self.trackletCount = 0
 
     def _generateTracklets(self, detectionList):
         tracklets = []
         for detection in detectionList:
-            if self.colour == 'ML':
-                colour = getColourML(detection, self.img)
-            if self.colour == 'Simple':
-                colour = getColourSimple(detection, self.img)
-            else:
-                colour = None
+            colour = self._getColour(detection)
             tracklet = Tracklet(self.trackletCount, detection, colour) #??????????????? review the detection formatting ----------------------------!
             tracklets.append(tracklet)
             self.trackletCount += 1
         return tracklets
 
-    def _updateTracks(self): # reassess inputs
+    def _updateTracks(self):
         confs = self._calculateConfidence()
-        # confs = self._calculateConfidence(False)
-        # print(confs)
+        # confs = self._calculateConfidence(False) # deciding how to sort, this approach has been removed
         self._assignTracklets(confs)
-
-        # assign tracklets/update position track=tracklet
         # create new tracks, if not assigned
         # remove old tracks, too long without update
-        self.tracklets = [] # remove when done
+        self.tracklets = []
 
     def _calculateConfidence(self):
-        # need identifier for if a tracklet has been assigned to potential and if it gets updated
         trackConfidence = []
         for track in self.tracks:
-            # trackConfidence[track.Id] = []
             for tracklet in self.tracklets:
                 conf = self._calculateWeightedConfidence(track, tracklet) # calculate confidence for each track tracklet pair
                 # if conf >= self.thres:
                 #     trackConfidence.append([track.Id, tracklet.Id, conf])
-                trackConfidence.append([track.Id, tracklet.Id, conf])
+                trackConfidence.append([track.Id, tracklet.Id, conf]) # potenitally convert to np array
         return trackConfidence
 
-    def _assignTracklets(self, confs): # --------------------------------------------------- Could add assignment options as well, hungarian etc...----------------#
+    def _assignTracklets(self, confs):
         if self.assign == 'greedy':
-            confs = sortHighest(confs)
+            confs = sortHighest(confs) # potenitally convert to np array and np sorting, investigate priority queues instead of sorting
             assignedTrackIds = set()
             assignedTrackletIds = set()
             for conf in confs:
@@ -125,13 +146,12 @@ class QuickTrack: # Quicktrack class contains all image parameters and the maxim
                     continue  
                 track = next((track for track in self.tracks if track.Id == conf[0]), None)
                 if track is not None:
-                    # Find the tracklet by ID
-                    tracklet = next((tracklet for tracklet in self.tracklets if tracklet.Id == conf[1]), None)
+                    tracklet = next((tracklet for tracklet in self.tracklets if tracklet.Id == conf[1]), None) 
                     if tracklet is not None:
                         track.updateTrack(tracklet)
                         assignedTrackIds.add(track.Id)
                         assignedTrackletIds.add(tracklet.Id)
-            # generate new tracks for unassigned
+            # generate new tracks for unassigned ------------------------------!
         # elif self.assign == 'hungarian':
         #     continue
 
@@ -155,22 +175,22 @@ class QuickTrack: # Quicktrack class contains all image parameters and the maxim
         confs_vital.append(confVital_a())
         confs_vital.append(confVital_b())
         # ------------------------------------------------------------------------------------- #
+        if len(self.weights) != len(confs):
+            raise ValueError("The number of inputted weights must match the number of non-vital functions called. Check the _calculate_weighted_confidence function in Quicktrack.py.")
+
         flag = False
         for conf in confs_vital:
             if conf != 1:
                 flag = True
                 break
 
-
-        if len(self.weights) == len(confs):
-            for conf, weight in zip(confs, self.weights):
-                total_conf += conf * weight
-            weighted_confidence = total_conf / total_weight
-            if flag:
-                weighted_confidence = weighted_confidence * self.vitalScale 
-            return weighted_confidence
-        else:
-            sys.exit('The number of inputted weights must match the number of non vital functions called, check _calculate_weighted_confidence function in Quicktrack.py')
+        for conf, weight in zip(confs, self.weights):
+            total_conf += conf * weight
+        weighted_confidence = total_conf / total_weight
+        if flag:
+            weighted_confidence = weighted_confidence * self.vitalScale 
+        return weighted_confidence
+            
    
     def plot_one_box(self, x, img, color=None, label=None, line_thickness=3):
         # Plots one bounding box on image img
@@ -184,3 +204,12 @@ class QuickTrack: # Quicktrack class contains all image parameters and the maxim
             c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
             cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
             cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+    def _getColour(self, detection):
+        if self.colour == 'ML':
+            colour = getColourML(detection, self.img)
+        if self.colour == 'simple':
+            colour = getColourSimple(detection, self.img)
+        else:
+            colour = None
+        return colour
--- run.py
+++ run.py
@@ -4,15 +4,16 @@ import cv2
 
 tracker = QT.QuickTrack() # Initialise the quicktracker
 
-model = yolov5.load('yolov5s.pt')
+model = yolov5.load('yolov5x6.pt')
 
 model.conf = 0.25  # NMS confidence threshold
 model.iou = 0.45  # NMS IoU threshold
 model.agnostic = False  # NMS class-agnostic
 model.multi_label = False  # NMS multiple labels per box
 model.max_det = 1000  # maximum number of detections per image
+model.classes = [2]
 
-print('innit')
+print('innit')  
 # Open the video file
 cap = cv2.VideoCapture('output.mp4')
 
@@ -28,7 +29,8 @@ while cap.isOpened():
     pred = results.pred[0]
     detectionList = pred.tolist()
     # print(detectionList)
-    tracker.update(detectionList, frame)
+    _ = tracker.update(detectionList, frame)
+    # print('test')
     # tracker.show()
     
     if cv2.waitKey(1) == ord('q'):
