a901325a930dffc757bf5851235bd02ec1933d96
SLocs123 <sam@thelockyers.net>
2024-04-23 15:31:41 +0100

    git pull

--- QT/QuickTrack.py
+++ QT/QuickTrack.py
@@ -163,11 +163,18 @@ class QuickTrack:
         elif self.assign == 'hungarian': # may not need unnassignedTracks
             matches = linear_sum_assignment(confs)
             unassignedTracks = [i for i in range(len(self.tracks)) if i not in matches[:, 0]]
+            unassignedTracklets = []
             for x, y in matches: 
                 if confs[x,y] > self.thres:
                     self.Tracks[x].updateTrack(self.tracklets[y])
+                    unassignedTracklets.append(y)
                 else:
                     unassignedTracks.append(x)
+            for i in unassignedTracklets:
+                tracklet = self.tracklets[i]
+                obj = [tracklet.bbox[0], tracklet.bbox[1], tracklet.bbox[2], tracklet.bbox[3], tracklet.conf, tracklet.cls]
+                self.tracks.append(Tracks(self.trackCount, obj, self.frame, tracklet.colour))
+                
 
     def __removeTracks(self):
         for item in self.tracks:
